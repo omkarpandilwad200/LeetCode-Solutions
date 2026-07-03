1d6fd0bdfcac5c75f05d10e29ae9f6682e89b1c8
Omkar Pandilwad <omkarpandilwad200@gmail.com>
2026-07-03 16:46:40 +0530

    Time: 113 ms (9.23%), Space: 78 MB (61.59%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,104 +1,61 @@
-// class Solution {
-//     public int largestRectangleArea(int[] heights) {
-//         int n=heights.length;
-//         if(n==1){
-//             if(heights[n-1]==0){
-//                 return 0;
-//             }
-//             return heights[n-1];
-//         }
-       
-//         Stack<Integer> st =new Stack<>();
-//         int [] nse=new int[n];
-//         int [] pse=new int[n];
-//         nse[n-1]=-1;
-//         st.push(n-1);
-//         //Next Smaller Element
-//         for(int i=n-2;i>=0;i--){
-//             while(!st.isEmpty() && heights[st.peek()]>heights[i]){
-//                 st.pop();
-//             }
-//             if(st.isEmpty()){
-//                 nse[i]=n;
-//             }
-//             else{
-//                 nse[i]=st.peek();
-//             }
-//             st.push(i);
-//         }
-//         while(st.size()>0){
-//             st.pop();
-//         }
-
-//         //previous Smaller Element
-//         st.push(0);
-//         pse[0]=-1;
-        
-//         for(int i=1;i<n;i++){
-//             while(!st.isEmpty()&&heights[st.peek()]>heights[i]){
-//                 st.pop();
-//             }
-//             if(st.isEmpty()){
-//                 pse[i]=n;
-//             }
-//             else{
-//                 pse[i]=st.peek();
-//             }
-//             st.push(i);
-//         }
-
-//         // maximum area of rectangle 
-    
-//         int max = -1;
-
-//         for(int i=0;i<n;i++){
-//             int area =heights[i]*(nse[i]-pse[i]-1);
-//             max=Math.max(max,area);
-//         }
-
-// return max;
-//     }
-// }
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        if (n == 0) return 0;
-
-        Stack<Integer> st = new Stack<>();
-        int[] nse = new int[n];
-        int[] pse = new int[n];
-
-        // Next Smaller Element (NSE)
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
+        int n=heights.length;
+        // if(n==1){
+        //     if(heights[n-1]==0){
+        //         return 0;
+        //     }
+        //     return heights[n-1];
+        // }
+       
+        Stack<Integer> st =new Stack<>();
+        int [] nse=new int[n];
+        int [] pse=new int[n];
+        nse[n-1]=n;
+        st.push(n-1);
+        //Next Smaller Element
+        for(int i=n-2;i>=0;i--){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
                 st.pop();
             }
-            nse[i] = st.isEmpty() ? n : st.peek();
+            if(st.isEmpty()){
+                nse[i]=n;
+            }
+            else{
+                nse[i]=st.peek();
+            }
             st.push(i);
         }
-
-        while (!st.isEmpty()) {
+        while(st.size()>0){
             st.pop();
         }
 
-        // Previous Smaller Element (PSE)
-        // Note: Using strict '>' here allows duplicate elements to extend 
-        // their boundaries properly across one of the two passes.
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
+        //previous Smaller Element
+        st.push(0);
+        pse[0]=-1;
+        
+        for(int i=1;i<n;i++){
+            while(!st.isEmpty()&&heights[st.peek()]>=heights[i]){
                 st.pop();
             }
-            pse[i] = st.isEmpty() ? -1 : st.peek();
+            if(st.isEmpty()){
+                pse[i]=-1;
+            }
+            else{
+                pse[i]=st.peek();
+            }
             st.push(i);
         }
 
-        // Maximum area of rectangle
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            int area = heights[i] * (nse[i] - pse[i] - 1);
-            max = Math.max(max, area);
+        // maximum area of rectangle 
+    
+        int max =-1;
+
+        for(int i=0;i<n;i++){
+            int area =heights[i]*(nse[i]-pse[i]-1);
+            max=Math.max(max,area);
         }
-        
-        return max;
+
+return max;
     }
 }
