6524e7f9402cffdf3b76285fbacef89aad0d46dc
Omkar Pandilwad <omkarpandilwad200@gmail.com>
2026-08-04 08:43:49 +0530

    Time: 32 ms (5.3%), Space: 45.5 MB (51.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,38 @@
 class Solution {
-    static HashMap<Character,Integer> mapFreq(String str){
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(!map.containsKey(ch)){
-                map.put(ch,1);
-            }
-            else{
-                int currfreq=map.get(ch);
-                map.put(ch,currfreq+1);
-            }
-            
-        }
-        return map;
-    }
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        HashMap<Character,Integer> mp1=mapFreq(s);
-        HashMap<Character,Integer> mp2=mapFreq(t);
+ static HashMap<Character,Integer>mapFreq(String str){
+     HashMap<Character,Integer> map=new HashMap<>();
+     for(int i=0;i<str.length();i++){
+         char ch=str.charAt(i);
+         if(!map.containsKey(ch)){
+             map.put(ch, 1);
+         }
+         else{
+             int currFre=map.get(ch);
+             map.put(ch, currFre+1);
+         }
+     }
+     return map;
+ }
+ 
+ public static boolean isAnagram(String s,String t){
+     if(s.length()!=t.length()){
+         return false;
+     }
+     HashMap<Character,Integer> map=mapFreq(s);
+     for(int i=0;i<t.length();i++){
+         char ch=t.charAt(i);
+         if(!map.containsKey(ch)) return false;
+         int currfreq=map.get(ch);
+         map.put(ch, currfreq-1);
+     }
+     for(var i:map.values()){
+         if(i!=0){
+             return false;
+         }
+     }
+     return true;
+     
+ }
 
-        return mp1.equals(mp2);
 
-
-        
-    }
 }
