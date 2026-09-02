03fef803962a5bbfd1cdf6fefcc7ad20adac1e0f
Omkar Pandilwad <omkarpandilwad200@gmail.com>
2026-09-02 19:01:47 +0530

    Time: 0 ms (100%), Space: 44 MB (9.64%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int height(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+    public List levelTraversal(TreeNode root, int n, List list) {
+
+        if (root == null)
+            return null;
+        if (n == 1) {
+            list.add(root.val);
+            return list;
+        }
+        levelTraversal(root.left, n - 1, list);
+        levelTraversal(root.right, n - 1, list);
+        return list;
+    }
+
+    public List levelTraversal2(TreeNode root, int n, List list) {
+        if (root == null) {
+            return null;
+        }
+        if (n == 1) {
+            list.add(root.val);
+            return list;
+
+        }
+        levelTraversal2(root.right, n - 1, list);
+        levelTraversal2(root.left, n - 1, list);
+        return list;
+
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        int level = 1 + height(root);
+        for (int i = 1; i <= level; i++) {
+            ArrayList<Integer> list = new ArrayList<>();
+            // if(root==null){
+            //     return ans;
+            // }
+            if (i % 2 != 0) {
+                levelTraversal(root, i, list);
+                ans.add(list);
+            } else {
+                levelTraversal2(root, i, list);
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+}
